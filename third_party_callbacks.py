77e0c874910a41980d619b71c8b254b5540c7562
Chris Priest <cp368202@ohiou.edu>
2013-08-31 22:17:58 -0700

    more db_session bugs

--- third_party_callbacks.py
+++ third_party_callbacks.py
@@ -2,13 +2,12 @@ from giotto import get_config
 from models import Library, UploadEngine
 from giotto_google.models import get_google_flow
 
-session = get_config('session')
-
 def dropbox_api_callback(user, access_token):
     """
     Handles the request that the user makes given by the dropbox API as part of
     the oauth process. This is the callback used in oauth1.
     """
+    session = get_config('db_session')
     library = Library.get(user.username)
     at = {'key': access_token.key, 'secret': access_token.secret}
     engine = UploadEngine(name="dropbox", connection_data=at, library=library)
@@ -23,6 +22,7 @@ def google_api_callback(user, credentials):
     back to this program, where `code` is exchanged for an auth token, and
     then stored.
     """
+    session = get_config('db_session')
     library = Library.get(user.username)
     engine = UploadEngine(name="googledrive", connection_data=credentials, library=library)
     session.add(engine)
